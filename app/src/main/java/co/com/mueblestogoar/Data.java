e9aa711cfbe2948f1a2b422440c87e90e78e2e2b
Oscar Javier Malagon Prieto <Oscar Javier Malagon Prieto>
2017-05-07 19:33:18 -0500

    Se implementa galería dinámica de objetos

--- app/src/main/java/co/com/mueblestogoar/Data.java
+++ app/src/main/java/co/com/mueblestogoar/Data.java
@@ -1,5 +1,14 @@
 package co.com.mueblestogoar;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ojmalagon on 10/04/2017.
  */
@@ -13,7 +22,209 @@ public class Data {
             R.drawable.mueblestogo};
 
     public final static String[] DESCRIPTIONS = {
-            "Sofa 1 QWERT"
+            "Sofa 1 QWERT"};
 
-    };
+
+    private List<Category> catalog;
+    private List<Item> itemsCategory;
+
+
+
+    public Data(){
+        //
+    }
+
+    public void LoadData(){
+        //Initi catalog & ítems
+
+        ObjectMapper mapper = new ObjectMapper();
+
+
+        String a = "{\"id\":1,\"name\":\"Salas\",\"image\":"+R.drawable.salacategory+","+
+                    "\"items\":[{\"id\":1,\"description\":\"SSSSSSSSS\",\"name\":\"Sofá cama\",\"dimensions\":\"1.70 X 1.20 x 0.70 cms\", \"image\":"+R.drawable.sofacama1+"},{\"id\":2,\"description\":\"SSSSSSSSS\",\"name\":\"Sofá cama extendido\",\"dimensions\":\"1.70 X 1.20 x 0.70 cms\", \"image\":"+R.drawable.cama1+"} ]" +
+                   "}";
+        Category cat=null;
+        try
+        {
+             cat =  mapper.readValue(a, Data.Category.class);
+        }
+        catch (JsonGenerationException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        catalog = new ArrayList<Category>();
+        itemsCategory = new ArrayList<Item>();
+
+        catalog.add(cat);
+/*
+        //Category 1
+        Category category = new Category();
+        category.setName("Salas");
+        category.setId((long) 1);
+        category.setImage(R.drawable.salacategory);
+        catalog.add(category);
+        //Item 1
+        Item item = new Item();
+        item.setId((long) 1);
+        item.setName("Sofá cama");
+        item.setDimensions("1.70 X 1.20 x 0.70 cms");
+        item.setImage(R.drawable.sofacama1);
+        itemsCategory.add(item);
+        //Item 2
+        item = new Item();
+        item.setId((long) 2);
+        item.setName("Sofá cama extendido");
+        item.setDimensions("1.70 X 1.20 x 0.70 cms");
+        item.setImage(R.drawable.cama1);
+        itemsCategory.add(item);
+        //Set Items 1 y 2 to Category 1
+        category.setItems(itemsCategory);
+
+        itemsCategory = new ArrayList<Item>();
+        //Category 2
+        category = new Category();
+        category.setName("Alcobas");
+        category.setId((long) 1);
+        category.setImage(R.drawable.alcobacategory);
+        catalog.add(category);
+        //Item 1
+        item = new Item();
+        item.setId((long) 1);
+        item.setName("Alcoba 1");
+        item.setDimensions("1.70 X 1.20 x 0.70 cms");
+        item.setImage(R.drawable.sofacama1);
+        itemsCategory.add(item);
+        //Item 2
+        item = new Item();
+        item.setId((long) 2);
+        item.setName("Alcoba 2");
+        item.setDimensions("1.70 X 1.20 x 0.70 cms");
+        item.setImage(R.drawable.cama1);
+        itemsCategory.add(item);
+        //Set Items 1 y 2 to Category 1
+        category.setItems(itemsCategory);*/
+    }
+
+    public List<Category> getCatalog() {
+        return catalog;
+    }
+
+    public void setCatalog(List<Category> catalog) {
+        this.catalog = catalog;
+    }
+
+    public List<Item> getItemsCategoria() {
+        return itemsCategory;
+    }
+
+    public void setItemsCategoria(List<Item> itemsCategory) {
+        this.itemsCategory = itemsCategory;
+    }
+
+
+    public static class Item{
+
+        private Long id;
+
+        private String description;
+
+        private String name;
+
+        private String dimensions;
+
+        private Integer image;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDimensions() {
+            return dimensions;
+        }
+
+        public void setDimensions(String dimensions) {
+            this.dimensions = dimensions;
+        }
+
+        public Integer getImage() {
+            return image;
+        }
+
+        public void setImage(Integer image) {
+            this.image = image;
+        }
+    }
+
+    public static class Category {
+
+        /*public Category(){
+            super();
+        }*/
+        private List<Item> items;
+
+        private Long id;
+
+        private String name;
+
+        private Integer image;
+
+        public List<Item> getItems() {
+            return items;
+        }
+
+        public void setItems(List<Item> items) {
+            this.items = items;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getImage() {
+            return image;
+        }
+
+        public void setImage(Integer image) {
+            this.image = image;
+        }
+    }
+
+
+
 }
+
--- app/src/main/java/co/com/mueblestogoar/MainActivity.java
+++ app/src/main/java/co/com/mueblestogoar/MainActivity.java
@@ -24,26 +24,47 @@ public class MainActivity extends AppCompatActivity {
     GridView gw;
     private SensorManager mSensorManager;
     private float azimuth;
+    private int index;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Load Data
+        Data data = new Data();
+        data.LoadData();
+
         gw = (GridView) findViewById(R.id.grPpal);
-        gw.setAdapter( new GridAdapter());
+        //getIntent().get
+
+        this.index = -1;
+        if(getIntent().getIntExtra("index",-1)!=-1){
+            //
+            this.index = getIntent().getIntExtra("index",0);
+        }
+        gw.setAdapter( new GridAdapter(data,this.index));
 
         gw.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(getApplicationContext(),ViewAR.class);
-                String p = Data.ITEM_NAMES[position].toString();
-                float orientation[] = new float[3];
-                float R[] = new float[9];
-                SensorManager.getOrientation(R, orientation);
-                azimuth = (orientation[0] < 0) ? 2 * (float)Math.PI + orientation[0] : orientation[0];
-                i.putExtra("model",p);
-                i.putExtra("azimuth", azimuth);
-                startActivity(i);
+
+                if( index==-1){
+                    Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                    i.putExtra("index",position);
+                    startActivity(i);
+                }
+                else {
+                    Intent i = new Intent(getApplicationContext(),ViewAR.class);
+                    String p = "";//Data.ITEM_NAMES[position].toString();
+                    float orientation[] = new float[3];
+                    float R[] = new float[9];
+                    SensorManager.getOrientation(R, orientation);
+                    azimuth = (orientation[0] < 0) ? 2 * (float)Math.PI + orientation[0] : orientation[0];
+                    i.putExtra("model",p);
+                    i.putExtra("azimuth", azimuth);
+                    startActivity(i);
+                }
+
             }
         });
     }
@@ -51,19 +72,49 @@ public class MainActivity extends AppCompatActivity {
 
     class GridAdapter extends BaseAdapter {
 
+        private Data DataGrid;
+        private int index;
+
+
+         public GridAdapter(Data data, int index){
+             DataGrid = data;
+             this.index = index;
+         }
+
         @Override
         public int getCount() {
-            return Data.IMAGE_IDS.length;
+
+            //return Data.IMAGE_IDS.length;
+            if(this.index==-1){
+                return DataGrid.getCatalog().size();
+            }
+            else{
+                return DataGrid.getCatalog().get(index).getItems().size();
+            }
+
         }
 
         @Override
         public Object getItem(int position) {
-            return Data.IMAGE_IDS[position];
+
+            //return Data.IMAGE_IDS[position];
+            if(this.index==-1){
+                return DataGrid.getCatalog().get(position);
+            }
+            else{
+                return DataGrid.getCatalog().get(index).getItems().get(position);
+            }
         }
 
         @Override
         public long getItemId(int position) {
-            return 0;
+
+            if(this.index==-1){
+                return DataGrid.getCatalog().get(position).getId();
+            }
+            else{
+                return DataGrid.getCatalog().get(index).getItems().get(position).getId();
+            }
         }
 
         @Override
@@ -71,8 +122,18 @@ public class MainActivity extends AppCompatActivity {
             convertView = getLayoutInflater().inflate(R.layout.single_grid, parent,false);
             ImageView iv = (ImageView) convertView.findViewById(R.id.imageView);
             TextView tv = (TextView) convertView.findViewById(R.id.textView);
-            iv.setImageResource(Data.IMAGE_IDS[position]);
-            tv.setText(Data.ITEM_NAMES[position]);
+
+            if(this.index==-1){
+                //return DataGrid.getCatalog().get(position).getId();
+                iv.setImageResource(DataGrid.getCatalog().get(position).getImage());
+                tv.setText(DataGrid.getCatalog().get(position).getName());
+            }
+            else{
+                //return DataGrid.getCatalog().get(index).getItems().get(position).getId();
+                iv.setImageResource(DataGrid.getCatalog().get(index).getItems().get(position).getImage());
+                tv.setText(DataGrid.getCatalog().get(index).getItems().get(position).getName());
+            }
+
             return convertView;
         }
     }
